e4f6daa3c52b64c0f7817d2f4d99b140d8a11fec
shenjingnan <sjn.code@gmail.com>
2025-03-14 13:38:41 +0800

    WIP

--- app/not-found.jsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-[70vh]">
-      <h1 className="text-4xl font-bold mb-4">404</h1>
-      <p className="text-xl">页面未找到</p>
-    </div>
-  )
-} 
